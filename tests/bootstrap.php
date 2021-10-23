8c06a55d8f3dc8432c8a52a8cb4480b7f6b58f7f
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2021-10-23 04:05:26 +0100

    testable

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,8 @@
 
 namespace YAPFtest;
 
+use YAPF\Core\ErrorControl\ErrorLogging;
+
 /*
     See spec.txt
     for load order for testing
@@ -9,3 +11,15 @@ namespace YAPFtest;
 error_reporting(E_ALL & ~E_NOTICE & ~E_USER_NOTICE);
 
 include("vendor/autoload.php");
+
+
+class ErrorLoggingTestClass extends ErrorLogging
+{
+    /**
+     * by default addError is protected
+     */
+    public function test_addError(string $fl = "", string $fn = "", string $er = "", array $ext = []): array
+    {
+        return $this->addError($fl, $fn, $er, $ext);
+    }
+}
